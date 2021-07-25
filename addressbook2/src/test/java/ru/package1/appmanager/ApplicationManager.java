264f00a0b39b5a935a23b2e600829c6e2a08da83
anjakolom <anjakolom@gmail.com>
2021-07-25 22:48:33 +0300

    Добавлен класс GroupModificationTests

--- addressbook2/src/test/java/ru/package1/appmanager/ApplicationManager.java
+++ addressbook2/src/test/java/ru/package1/appmanager/ApplicationManager.java
@@ -47,7 +47,6 @@ public class ApplicationManager {
     }
 
 
-
     public GroupHelper getGroupHelper() {
         return groupHelper;
     }
---
+++ addressbook2/src/test/java/ru/package1/test/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package ru.package1.test;
+
+import org.testng.annotations.Test;
+import ru.package1.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification(){
+
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("New_groups_2", "222", "222"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+
+}
