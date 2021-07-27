b2af589faf6a2f1ca89732ac0b48b6a19e77faf1
anjakolom <anjakolom@gmail.com>
2021-07-27 09:47:42 +0300

    Добавлен выбор браузера (Chrome, Firefox, IE)

--- addressbook2/src/test/java/ru/package1/test/TestBase.java
+++ addressbook2/src/test/java/ru/package1/test/TestBase.java
@@ -1,12 +1,13 @@
 package ru.package1.test;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.package1.appmanager.ApplicationManager;
 
 public class TestBase {
-
-    protected final ApplicationManager app = new ApplicationManager();
+    //Драйвер браузера: BrowserType.FIREFOX,BrowserType.IE,BrowserType.CHROME
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
