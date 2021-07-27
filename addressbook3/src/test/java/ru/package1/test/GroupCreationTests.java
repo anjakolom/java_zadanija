94239f8bbaab8dc480d60b3ec53bf196dc35a754
anjakolom <anjakolom@gmail.com>
2021-07-27 16:08:46 +0300

    Добавлен метод equals вместо "=="

---
+++ addressbook3/src/test/java/ru/package1/test/GroupCreationTests.java
@@ -0,0 +1,20 @@
+package ru.package1.test;
+
+import org.testng.annotations.*;
+import ru.package1.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+    @Test
+    public void testGroupCreation() throws Exception {
+
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("New_groups_1", "1111", "222"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
+
+    }
+
+
+}
---
+++ addressbook3/src/test/java/ru/package1/test/NewContactTest.java
@@ -0,0 +1,17 @@
+package ru.package1.test;
+
+import org.testng.annotations.Test;
+import ru.package1.model.ContactData;
+
+public class NewContactTest extends TestBase {
+
+    @Test
+    public void testNewContact() throws Exception {
+        app.getContactHelper().initNewContact();
+        app.getContactHelper().fillContactForm(new ContactData("FirstName", "MiddleName", "LastName", "Nickname", "Title", "Company", "Address", "+79260211966", "Work", "email", "10", "november", "1982", "New_groups_1"));
+        app.getContactHelper().submitContactCreation();
+
+    }
+
+
+}
