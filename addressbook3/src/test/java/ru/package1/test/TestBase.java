f9ee35cec377c17447bf9ad59d29dd54cacaff43
anjakolom <anjakolom@gmail.com>
2021-08-13 16:06:45 +0300

    Доработан запуск всех тестов одновременно

--- addressbook3/src/test/java/ru/package1/test/TestBase.java
+++ addressbook3/src/test/java/ru/package1/test/TestBase.java
@@ -1,20 +1,20 @@
 package ru.package1.test;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.package1.appmanager.ApplicationManager;
 
 public class TestBase {
     //Драйвер браузера: BrowserType.FIREFOX,BrowserType.IE,BrowserType.CHROME
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static  ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterSuite
     public void tearDown() throws Exception {
         app.stop();
 
