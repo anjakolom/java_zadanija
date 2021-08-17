af191ea2b71387763985f144f4ac7c121394000f
anjakolom <anjakolom@gmail.com>
2021-08-17 16:19:29 +0300

    Добавлено кэширование при работе с группами

--- addressbook3/src/test/java/ru/package1/appmanager/GroupHelper.java
+++ addressbook3/src/test/java/ru/package1/appmanager/GroupHelper.java
@@ -52,6 +52,7 @@ public class GroupHelper extends HelperBase {
         initGroupCreation();
         fillGroupForm(group);
         submitGroupCreation();
+        groupCashe = null;
         appl.goTo().groupPage();
     }
 
@@ -60,6 +61,7 @@ public class GroupHelper extends HelperBase {
         initGroupModification();
         fillGroupForm(group);
         submitGroupModification();
+        groupCashe = null;
         appl.goTo().groupPage();
 
     }
@@ -67,6 +69,7 @@ public class GroupHelper extends HelperBase {
     public void delete(GroupData group) {
         selectGroupByID(group.getId());
         deleteSelectedGroups();
+        groupCashe = null;
         appl.goTo().groupPage();
     }
 
@@ -78,18 +81,22 @@ public class GroupHelper extends HelperBase {
         return wd.findElements(By.name("selected[]")).size();
     }
 
+    private Groups groupCashe = null;
 
     public Groups all() {
-        Groups groups = new Groups();
+        if (groupCashe != null){
+            return new Groups(groupCashe);
+        }
+        groupCashe = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         System.out.println("Получили список групп: ");
         for (WebElement element : elements) {
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            groups.add(new GroupData().withId(id).withName(name));
+            groupCashe.add(new GroupData().withId(id).withName(name));
             System.out.println(name+"; ");
         }
-        return groups;
+        return new Groups(groupCashe);
     }
 
 
