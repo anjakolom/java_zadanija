cc585aa360bbc79f54c78e34b084a476a160a29a
anjakolom <anjakolom@gmail.com>
2021-08-23 10:01:47 +0300

    Добавлена работа с библиотекой jcommander

--- addressbook3/src/test/java/ru/package1/generators/GroupDataGenerator.java
+++ addressbook3/src/test/java/ru/package1/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.package1.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.package1.model.GroupData;
 
 import java.io.File;
@@ -9,16 +12,38 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GroupDataGenerators {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
+public class GroupDataGenerator {
 
-        List<GroupData> groups = generateGroups(count);
-        save(groups, file);
+    @Parameter (names = "-c", description = "Group count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+        GroupDataGenerator generator = new GroupDataGenerator();
+        //int count = Integer.parseInt(args[0]);
+        //File file = new File(args[1]);
+        JCommander jCommander = new JCommander(generator);
+        try{
+        jCommander.parse(args);
+        } catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+
+
+        generator.run();
+
+
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        save(groups, new File (file));
+    }
+
+    private  void save(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group:groups){
@@ -28,7 +53,7 @@ public class GroupDataGenerators {
 
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private  List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i=0;i<count; i++){
             groups.add(new GroupData().withName(String.format("Groups_generate_%s",i))
