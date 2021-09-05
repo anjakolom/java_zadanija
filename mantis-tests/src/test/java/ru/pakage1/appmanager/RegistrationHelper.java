36588c121a8193b7eb7db4227120f7d7fb7d01d0
anjakolom <anjakolom@gmail.com>
2021-09-05 23:11:12 +0300

    Добавлен HelperBase

--- mantis-tests/src/test/java/ru/pakage1/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/pakage1/appmanager/RegistrationHelper.java
@@ -1,17 +1,22 @@
 package ru.pakage1.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
-    private final ApplicationManager app;
-    private WebDriver wd;
+public class RegistrationHelper extends HelperBase{
+    //private final ApplicationManager app;
+   // private WebDriver wd;
 
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        wd = app.getDriver();
+        super(app);
+      //  wd = app.getDriver();
     }
 
     public void start(String username, String email) {
         wd.get(app.getProperty("web.baseUrl")+"/signup.php");
+        type(By.name("username"),username);
+        type(By.name("email"),email);
+        click(By.cssSelector("input[value="Зарегистрироваться"]"));
+
     }
 }
